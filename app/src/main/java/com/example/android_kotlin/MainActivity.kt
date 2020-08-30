1ff74435b192badcb4ea4900259c112d1f99b8b7
Witaliikaplun <vitalijkaplun@gmail.com>
2020-08-30 20:47:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android_kotlin/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.android_kotlin
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btn = findViewById<Button>(R.id.btn1)
+        btn.setOnClickListener { Toast.makeText(applicationContext, "нажали кнопку", Toast.LENGTH_LONG).show() }
+    }
+}
